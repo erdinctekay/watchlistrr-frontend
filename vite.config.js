b5f8c0ecdd1a8e59c40c430f30cc54b78c019a8e
Erdinc Tekay <erdinctekay@gmail.com>
2023-03-09 21:27:29 +0300

    build optimization added with minification and stripping

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,35 @@
 import { defineConfig } from 'vite'
-import path from 'path'
 import vue from '@vitejs/plugin-vue'
+
+import path from 'path'
+
 import autoprefixer from 'autoprefixer'
+import strip from '@rollup/plugin-strip'
+import htmlMinifier from 'html-minifier'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [
+		vue(),
+		strip({
+			// Remove console.log statements and comments
+			functions: ['console.log'],
+			inline: true,
+			comments: true,
+			include: ['**/*.js', '**/*.html', '**/*.css', '**/*.vue'],
+			exclude: 'node_modules/**',
+		}),
+		{
+			name: 'html-minifier',
+			transformIndexHtml(html) {
+				return htmlMinifier.minify(html, {
+					collapseWhitespace: true,
+					removeComments: true,
+					minifyCSS: true,
+				})
+			},
+		},
+	],
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),

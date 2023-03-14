79c37a268ca0a8b83b893c9cf99d146ea8082079
Erdinc Tekay <erdinctekay@gmail.com>
2023-03-14 17:00:36 +0300

    private watchlist indicator isolated as component

---
+++ src/components/indicators/PrivateItemIndicator.vue
@@ -0,0 +1,33 @@
+<template>
+	<span style="padding-top: 3px" :style="{ width: `${IconWidth}px` }">
+		<span class="d-flex hover-highlight-icon align-items-center w-100 justify-content-end m-n1">
+			<i
+				@click.stop.prevent=""
+				class="bi bi-eye-slash-fill filled-icon p-1 me-n1 cursor-pointer"
+				style="font-size: 1rem"
+				data-bs-toggle="popover"
+				data-bs-trigger="hover"
+				data-bs-placement="bottom"
+				data-bs-content="This watchlist is private to you 
+                    and isn't visible for other users."
+			></i>
+		</span>
+	</span>
+</template>
+<script setup>
+	import { Popover } from 'bootstrap'
+	import { onMounted } from 'vue'
+
+	import { enablePopovers } from '@/helpers/ui'
+
+	const props = defineProps({
+		IconWidth: {
+			type: Number,
+			default: 36,
+		},
+	})
+
+	onMounted(() => {
+		enablePopovers(Popover)
+	})
+</script>

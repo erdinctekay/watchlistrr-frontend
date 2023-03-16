79f300bb043d977f0a63d84231bf189e3a221504
Erdinc Tekay <erdinctekay@gmail.com>
2023-03-16 18:10:08 +0300

    nothing found indicator view logic improved with conditionals

--- src/components/indicators/NothingFoundIndicator.vue
+++ src/components/indicators/NothingFoundIndicator.vue
@@ -3,9 +3,15 @@
 		<div class="container-fluid container-fluid-xxl py-2 px-3 px-sm-5 row g-0 m-auto">
 			<div class="d-flex flex-column justify-content-center align-items-center" style="min-height: 60vh">
 				<h1 class="fw-bold">OOPS!</h1>
-				<span class="h4 mb-5">Nothing found here :(</span>
+
+				<span v-if="activeSortOptions.searchQuery !== ''" class="h4 mb-5">
+					Sorry, we couldn't find any matching records.
+				</span>
+
+				<span v-else class="h4 mb-5">Nothing to show here.</span>
+
 				<button-constructor
-					v-if="currentPage.name !== 'home'"
+					v-if="currentPage.name !== 'home' && activeSortOptions.searchQuery === ''"
 					@click="returnPage('home')"
 					:mainColor="'primary'"
 					:mainClass="'rounded-pill py-1'"
@@ -23,8 +29,10 @@
 	import { storeToRefs } from 'pinia'
 	import { router } from '@/helpers'
 	import { useRouteStore } from '@/stores/RouteStore'
+	import { useSortStore } from '@/stores/SortStore'
 
 	const { currentPage } = storeToRefs(useRouteStore())
+	const { activeSortOptions } = useSortStore()
 
 	const { returnPage } = router
 </script>

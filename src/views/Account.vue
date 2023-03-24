25d7e5c30f23ce08b0f9445b2c71c08c334f2e1f
Erdinc Tekay <erdinctekay@gmail.com>
2023-03-24 11:00:29 +0300

    updated account page

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -2,10 +2,13 @@
 	<section class="col-12 my-4">
 		<div class="container-fluid container-fluid-xxl py-2 px-3 px-sm-5 row g-0 m-auto">
 			<h3>This is account page</h3>
+			<span class="">All account related informations and settings will be displayed here on next versions.</span>
 		</div>
 	</section>
 </template>
 
-<script></script>
+<script setup>
+	//
+</script>
 
 <style></style>
